99cc9f3db2dac85c164673a2c5c1bd62446c30e2
LuizFernandoFS <lffs2000@outlook.com>
2023-11-14 21:25:39 -0300

    Concluído v1.0 do Quiz

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -11,12 +11,17 @@ export function GameOver(game: GameOverProps) {
         <div className="container">
             <div className="game-over">
                 <p>Pontuação Final: {game.pontuacaoFinal}</p>
-                <p>Acertou {game.acertos} de {game.quantidadeDePerguntas} questões.</p>
+                <p>(Acertou {game.acertos} de {game.quantidadeDePerguntas} questões)</p>
                 { 
                     game.acertos >= 7 ?
-                    <p>Muito bom!!! você acertou a maioria das perguntas.</p>
+                    <p style={{
+                        color: "#00CC00"
+                    }}>Muito bem!!! você acertou a maioria das perguntas.</p>
                     : 
-                    <p>Precisa melhorar! você errou um número razoável de perguntas.</p> } 
+                    <p style={{
+                        color: "#d90d0d"
+                    }}>Precisa melhorar! você errou um número razoável de perguntas.</p>
+                } 
             </div>
         </div>
     );
